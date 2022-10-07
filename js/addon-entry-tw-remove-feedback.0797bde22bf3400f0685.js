9c30e860671ff751fee11a5565eb93b0c57fc882
AlexSchoolOH <71141838+AlexSchoolOH@users.noreply.github.com>
2022-10-06 21:34:55 -0400

    Add files via upload

---
+++ js/addon-entry-tw-remove-feedback.0797bde22bf3400f0685.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[48],{1583:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",(function(){return o}));var c=s(1584);const o={"remove-feedback.css":s.n(c).a}},1584:function(n,e,s){(n.exports=s(9)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
